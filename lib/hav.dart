8345d4016599ff9fb0f5ac16405b9d016e373770
العربي <mm8645007@gmail.com>
2021-08-20 14:32:05 +0200

    second

---
+++ lib/hav.dart
@@ -0,0 +1,169 @@
+import 'package:mohamed/sizeConfig.dart';
+import 'package:flutter/material.dart';
+class HavScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+
+    return new Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: new Image.asset("images/mb.png",height:getProportionateScreenHeight(90.00) ,width:getProportionateScreenWidth(60.00)),
+        actions: [
+          new Text("mohamed",style:TextStyle(color: Colors.black)),
+
+
+        ],
+
+
+      ),
+      body: new Container(
+        color: Colors.white,
+
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+
+          children:[
+          new Container(
+            color: Colors.white,
+            alignment: Alignment.center,
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ListTile(
+                  leading: Icon(
+                    Icons.animation,
+                    color: Theme.of(context).accentColor,
+                    size: 25,
+                  ),
+
+                  title: Text(
+                    ("رصيد الحساب "),
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 18,
+                    color: Colors.greenAccent,
+                  ),
+
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.style,
+                    color: Theme.of(context).accentColor,
+                    size: 25,
+                  ),
+                  title: Text(
+                    ("عدد الطلبات"),
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 18,
+                    color: Colors.greenAccent,
+                  ),
+
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.announcement,
+                    color: Theme.of(context).accentColor,
+                    size: 25,
+                  ),
+                  title: Text(
+                    ("تقيمات الخدمات"),
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 18,
+                    color: Colors.greenAccent,
+                  ),
+
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.portrait,
+                    color: Theme.of(context).accentColor,
+                    size: 25,
+                  ),
+                  title: Text(
+                    (" ملاحظات المستخدمين"),
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 18,
+                    color: Colors.greenAccent,
+                  ),
+
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.portrait,
+                    color: Theme.of(context).accentColor,
+                    size: 25,
+                  ),
+                  title: Text(
+                    (" الكوبونات"),
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 18,
+                    color: Colors.greenAccent,
+                  ),
+
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.portrait,
+                    color: Theme.of(context).accentColor,
+                    size: 25,
+                  ),
+
+                  title: Text(
+                    (" أعدادات"),
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 18,
+                    color: Colors.greenAccent,
+                  ),
+
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.portrait,
+                    color: Theme.of(context).accentColor,
+                    size: 25,
+                  ),
+
+                  title: Text(
+                    (" طريقة السداد"),
+                    style: const TextStyle(fontSize: 15),
+                  ),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 18,
+                    color: Colors.greenAccent,
+                  ),
+
+                ),
+
+              ],
+            ),
+          ),
+
+           ],
+        ),
+      ),
+
+
+
+    );
+  }
+}
---
+++ lib/search.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:mohamed/sizeConfig.dart';
+class SearchScreen extends StatelessWidget {
+TextEditingController _usernameController = new TextEditingController();
+TextEditingController _passwordController = new TextEditingController();
+
+void _clear(){
+  setState((){
+    _usernameController.clear();
+    _passwordController.clear();
+  });
+}
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+
+    return new Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Center(child: Text("Login App", style: TextStyle(color: Colors.black),)),
+
+      ),
+      body: new Container(
+        alignment: Alignment.topCenter,
+        child: new Column(
+          children: [
+            new Image.asset("images/mk.png",height:getProportionateScreenHeight(100.00) ,width:getProportionateScreenWidth(100.00)),
+            new Container(
+              height: 180,
+              width: 380,
+              child: new Column(
+                children: [
+                  new TextField(
+                    controller: _usernameController,
+                    decoration: new InputDecoration(
+                      hintText: " enter your username",
+                      icon: new Icon(Icons.person),
+
+                    ),
+
+                  ),
+                  new TextField(
+                    controller: _passwordController,
+                    decoration: new InputDecoration(
+                      hintText: " enter your password",
+                      icon: new Icon(Icons.lock),
+
+                  ),
+                  ),
+                  new Padding(padding: new EdgeInsets.all(15.0)),
+
+
+                  new Center(
+                    child: new Row(
+                      children: [
+                        new Container(
+                          margin: const EdgeInsets.only(left:37.0),
+                           child: new RaisedButton(
+                            onPressed: ()=>debugPrint("Login"),
+                            child: new Text("Submit",style: new TextStyle(color: Colors.white,fontSize: 20.5),),
+                            color: Colors.red,
+
+                          ),
+                        ),
+                        new Container(
+                          margin: const EdgeInsets.only(left:100.0),
+                          child: new RaisedButton(
+                            onPressed: _clear,
+                            child: new Text("Clear",style: new TextStyle(color: Colors.white,fontSize: 20.5),),
+                            color: Colors.red,
+
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            new Container(
+              alignment: Alignment.center,
+              child:new Text("Welcome In our App",style: new TextStyle(color: Colors.black,fontSize: 24.0,fontWeight: FontWeight.w700)),
+
+            ),
+          ],
+        ),
+
+      ),
+
+
+
+
+    );
+  }
+
+  void setState(Null Function() param0) {}
+
+
+}
